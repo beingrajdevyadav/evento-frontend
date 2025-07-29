a467cee05f89d08224a474ec713cedf76cca068b
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-29 20:42:49 +0530

    app routing created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import EventDetails from './pages/EventDetails'
+import PageNotFound from './pages/PageNotFound'
+
 
 function App() {
 
@@ -7,6 +15,15 @@ function App() {
   return (
     <>
       <h1>Evento</h1>
+
+      <Routes>
+        <Route path='/' element={<Home/> } />
+        <Route path='/login' element={<Login/> } />
+        <Route path='/register' element={<Register/> } />
+        <Route path='/event/:id' element={<EventDetails/> } />
+        <Route path='/dashboard' element={<Dashboard/> } />
+        <Route path='*' element={<PageNotFound/> } />
+      </Routes>
     </>
   )
 }
