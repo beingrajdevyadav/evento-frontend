8b4fe6224a7b3bf3f9aa275cdbd8e8e9a1102886
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-29 21:48:42 +0530

    redux store created and app wrapped.

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice.js";
+import eventReducer from "../features/event/eventSlice.js"
+
+
+const store = configureStore({
+    reducer:{
+        auth:authReducer,
+        event: eventReducer,
+    }
+});
+
+
+export default store;
