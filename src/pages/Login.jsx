911d7352f724c1c06661d67ac463074e5f067de8
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-30 10:09:41 +0530

    login page created.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { loginUser } from '../features/auth/authSlice';
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const {user, loading} = useSelector((state)=>state.auth);
+  const [form, setForm] = useState({email: "", password: ""})
+  
+  console.log(user);
+  const handleChange = (e)=>setForm({...form, [e.target.name]: e.target.value});
+
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    dispatch(loginUser(form));
+  };
+
+
   return (
-    <div>Login</div>
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <input type="email" placeholder='Enter Your Email' name='email' value={form.email} onChange={handleChange} />
+        <input type="password" placeholder='Enter Password' name='password' value={form.password} onChange={handleChange} />
+
+        <button>{loading ? "Logging in...": "Login"}</button>
+      </form>
+    </div>
   )
 }
 
