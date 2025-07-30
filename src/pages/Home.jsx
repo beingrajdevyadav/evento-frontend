59b8f52ede1d1d3fb9a11dd9a3e6502b2ef064ed
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-30 11:34:11 +0530

    home page updated.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchEvents } from '../features/event/eventSlice'
+import EventCard from '../components/EventCard'
+
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const {events, loading, error} = useSelector((state)=>state.event);
+
+  useEffect(()=>{
+    dispatch(fetchEvents());
+  }, [dispatch]);
+
+  if(loading) return <p>Loading events...</p>;
+
+  if(error) return <p>{error}</p>;
+
   return (
-    <div>Home</div>
+    <div>
+      <h2>Upcoming Events</h2>
+      <hr />
+
+      {
+        events.length === 0 ? ( 
+          <p> No events available </p>
+        ):
+      (
+        events.map((e)=><EventCard key={e._id} event={e}/>)
+      )
+      }
+    </div>
   )
 }
 
