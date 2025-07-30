e4b4e575e5b72a8a1dc4d09d98f3924a9912108c
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-30 09:00:50 +0530

    authSlice created.

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -16,3 +16,47 @@ export const registerUser = createAsyncThunk("auth/register", async (formData) =
     const res = await API.post("/auth/register", formData);
     return res.data;
 })
+
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState:{
+        user: JSON.parse(localStorage.getItem("user")) || null,
+        loading: false,
+        error: null,
+    },
+
+    reducers: {
+        logout: (state)=>{
+            state.user = null;
+            localStorage.removeItem("user");
+        },
+    },
+    extraReducers: (builder)=>{
+        builder.addCase(loginUser.pending, (state)=>{
+            state.loading = true;
+        })
+        .addCase(loginUser.fulfilled, (state, action)=>{
+            state.loading = false;
+            state.user = action.payload;
+        })
+        .addCase(loginUser.rejected, (state)=>{
+            state.loading = false;
+            state.error = "Login failed"
+        })
+        .addCase(registerUser.pending, (state)=>{
+            state.loading = true;
+        })
+        .addCase(registerUser.fulfilled, (state)=>{
+            state.loading = false;
+
+        })
+        .addCase(registerUser.rejected, (state)=>{
+            state.loading = false;
+            state.error = "Registration failed";
+        });
+    },
+});
+
+export const {logout} = authSlice.actions;
+export default authSlice.reducer;
